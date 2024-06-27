26f052634623a2c4a4357d223b38bc1d4f7941f8
simoneniro17 <simoneniro17@gmail.com>
2024-06-27 09:50:17 +0200

    JournalTest updated for improving mutation score

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/JournalTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/JournalTest.java
@@ -1,13 +1,18 @@
 package org.apache.bookkeeper.bookie;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.io.File;
 import java.util.*;
 
+import static org.apache.commons.lang.ArrayUtils.reverse;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(value = Parameterized.class)
 public class JournalTest {
@@ -28,7 +33,8 @@ public class JournalTest {
         NOT_EXISTING_DIR,
         EMPTY_DIR,
         ONE_LOG_FILE_DIR,
-        NO_LOG_FILES_DIR;
+        NO_LOG_FILES_DIR,
+        TWO_LOG_FILES_DIR;
         
         private final String USER_DIR = System.getProperty("user.dir");
         
@@ -42,6 +48,8 @@ public class JournalTest {
                     return new File(USER_DIR, "src/test/resources/journal/one_log_file_dir");
                 case NO_LOG_FILES_DIR:
                     return new File(USER_DIR, "src/test/resources/journal/no_log_files_dir");
+                case TWO_LOG_FILES_DIR:
+                    return new File(USER_DIR, "src/test/resources/journal/two_log_files_dir");
                 default:
                     return null;
             }
@@ -79,6 +87,21 @@ public class JournalTest {
         NO
     }
     
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        
+        if(journalDir != null && Objects.equals(journalDir.getPath(), JournalDirType.TWO_LOG_FILES_DIR.getJournalDir().getPath())) {
+            File[] files = journalDir.listFiles();
+            
+            if(files != null) {
+                reverse(files);
+                journalDir = Mockito.spy(journalDir);
+                doReturn(files).when(journalDir).listFiles();
+            }
+        }
+    }
+    
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
@@ -87,8 +110,10 @@ public class JournalTest {
                 {JournalDirType.EMPTY_DIR.getJournalDir(), null, new ArrayList<Long>(), expectExceptionType.NO},
                 {JournalDirType.NO_LOG_FILES_DIR.getJournalDir(), JournalIdFilterType.PIPPO_FILTER.getJournalIdFilter(), new ArrayList<Long>(), expectExceptionType.NO},
                 {JournalDirType.ONE_LOG_FILE_DIR.getJournalDir(), JournalIdFilterType.JOURNAL_ROLLING_FILTER.getJournalIdFilter(), new ArrayList<>(Collections.singletonList(7L)), expectExceptionType.NO},
+                
                 {JournalDirType.ONE_LOG_FILE_DIR.getJournalDir(), null, new ArrayList<>(Collections.singletonList(7L)), expectExceptionType.NO},
-                {JournalDirType.ONE_LOG_FILE_DIR.getJournalDir(), JournalIdFilterType.PIPPO_FILTER.getJournalIdFilter(), new ArrayList<Long>(), expectExceptionType.NO}
+                {JournalDirType.ONE_LOG_FILE_DIR.getJournalDir(), JournalIdFilterType.PIPPO_FILTER.getJournalIdFilter(), new ArrayList<Long>(), expectExceptionType.NO},
+                {JournalDirType.TWO_LOG_FILES_DIR.getJournalDir(), null, new ArrayList<>(Arrays.asList(1L, 2L)), expectExceptionType.NO}
         });
     }
     
